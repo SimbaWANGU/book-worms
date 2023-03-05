726c61f3160510921b815cd7e6f4d4d33f920590
Simba Wangu <simbawangulu@gmail.com>
2023-03-05 17:04:21 +0300

    import images and fonts as constants in a common folder to prevent bloating code with imports and for easier use

---
+++ src/constants/images.ts
@@ -0,0 +1,5 @@
+import Image1 from '../../assets/images/onboardone.png'
+import Image2 from '../../assets/images/onboardtwo.png'
+import Image3 from '../../assets/images/onboardthree.png'
+
+export { Image1, Image2, Image3}
