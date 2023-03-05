67b1a9f77727cb902b6f29570feff2690d6204ad
Simba Wangu <simbawangulu@gmail.com>
2023-03-05 17:11:06 +0300

    fix linting errors

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -2,4 +2,4 @@ import Image1 from '../../assets/images/onboardone.png'
 import Image2 from '../../assets/images/onboardtwo.png'
 import Image3 from '../../assets/images/onboardthree.png'
 
-export { Image1, Image2, Image3}
+export { Image1, Image2, Image3 }
