7b4aacf8d5805a1d34f4016c578a2bb846a66a96
Simba Wangu <simbawangulu@gmail.com>
2023-03-06 13:20:48 +0300

    delete unneeded files while adding navigation

--- src/constants/fonts.ts
+++
@@ -1,5 +0,0 @@
-import Family1 from '../../assets/fonts/FontFamily1.ttf'
-import Family2 from '../../assets/fonts/FontFamily2.ttf'
-import Family3 from '../../assets/fonts/FontFamily3.ttf'
-
-export { Family1, Family2, Family3 }
